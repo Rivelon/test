f81440237ebdceb06bb2efd0fd2bab280859aef3
Rivelon <rivelon22@gmail.com>
2022-12-12 23:57:35 +0300

    update js code and add config file

--- script.js
+++ script.js
@@ -9,13 +9,19 @@ const personalMovieDB = {
 };
 
 
-const a = prompt('Один из просмотренный фильмов?' , ''),
-      b = prompt('Как вы его оцените?'),
-      c = prompt('Один из просмотренный фильмов?' , ''),
-      d = prompt('Как вы его оцените?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+   const a = prompt('Один из просмотренный фильмов?' , ''),
+         b = prompt('Как вы его оцените?');        
+         
+         if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+         } else { 
+            console.log('error');
+            i--;
+  }
+}
 
 
 console.log(personalMovieDB);
