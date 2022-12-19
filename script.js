bc2fed29cc200282b485691c7a0205102f7ba8a0
Rivelon <rivelon22@gmail.com>
2022-12-19 14:42:08 +0300

    update js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели', '');
+let numberOfFilms;
+
+function start() {
+   numberOfFilms = +prompt('Сколько фильмов вы уже смотрели', '');
+   while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+      numberOfFilms = +prompt('Сколько фильмов вы уже смотрели', '');
+   }
+}
+
+start();
+
 const personalMovieDB = {
    count : numberOfFilms,
    movies : {},
@@ -8,20 +18,34 @@ const personalMovieDB = {
    privat : false
 };
 
-
-
-for (let i = 0; i < 2; i++){
-   const a = prompt('Один из просмотренный фильмов?' , ''),
-         b = prompt('Как вы его оцените?');        
-         
-         if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-         } else { 
-            console.log('error');
-            i--;
-  }
+function rememberMyFilms(){
+   for (let i = 0; i < 2; i++){
+      const a = prompt('Один из просмотренный фильмов?' , ''),
+            b = prompt('Как вы его оцените?');        
+            
+            if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+               personalMovieDB.movies[a] = b;
+               console.log('done');
+            } else { 
+               console.log('error');
+               i--;
+     }
+   }
 }
 
+rememberMyFilms();
+
+function detectPersonalLevel(){
+   if (personalMovieDB.count < 10 ){console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+  console.log("Вы классический зритель"); 
+} else if (personalMovieDB.count >= 30){
+   console.log("Вы киноман");
+} else {
+   console.log("Произошла ошибка");
+}
+}
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
